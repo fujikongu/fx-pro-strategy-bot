5dacdcd2fdca5d72911e6d4202e814b0678bd5a7
fujikongu <fujikonbu0507@gmail.com>
2025-06-16 20:31:06 +0900

    Add files via upload

---
+++ html_form_large_button.py
@@ -0,0 +1,22 @@
+
+HTML_FORM = """
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>ランダムパスワード発行フォーム</title>
+    <style>
+        .big-button {
+            font-size: 3em;
+            padding: 1em 2em;
+        }
+    </style>
+</head>
+<body>
+    <h2>🔐 ランダムパスワード発行フォーム</h2>
+    <form method="POST">
+        <button class="big-button">🔄 発行する</button>
+    </form>
+</body>
+</html>
+"""
